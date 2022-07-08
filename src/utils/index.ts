3cdd1f9094efb3fa53c5862bec6f4ff1d7903dd8
YernarT <toktaryernar@gmail.com>
2022-07-08 23:28:01 +0600

    init: :rainbow: 工具函数

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export type { localStorageProperties } from './localStorage';
+export { localStorage } from './localStorage';
+export { default as getLatestState } from './getLatestState';
+
+export type { allowedRole } from './role';
+export { role } from './role';
+export { default as sleep } from './sleep';
