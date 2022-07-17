03bdad0fb99b2f6c601dd4a98329e4bd44d55cbf
YernarT <toktaryernar@gmail.com>
2022-07-17 17:43:12 +0600

    fix: :bug: remove some unused codes

--- src/routes/user-routes.tsx
+++ src/routes/user-routes.tsx
@@ -6,17 +6,17 @@ import { sleep, role } from '@/utils';
 import { CommonLoading } from '@/loading';
 
 const userRoutes: RouteProps[] = [
-	{
-		path: ['/user', '/user/home'],
-		component: lazy(async () => {
-			await sleep(360);
-			return import('@/pages/user/HomePage');
-		}),
-		fallback: <CommonLoading />,
-		auth: true,
-		role: [role.USER],
-		title: 'Home | React Template',
-	},
+	// {
+	// 	path: ['/user', '/user/home'],
+	// 	component: lazy(async () => {
+	// 		await sleep(360);
+	// 		return import('@/pages/user/HomePage');
+	// 	}),
+	// 	fallback: <CommonLoading />,
+	// 	auth: true,
+	// 	role: [role.USER],
+	// 	title: 'Home | React Template',
+	// },
 ];
 
 export default userRoutes;
